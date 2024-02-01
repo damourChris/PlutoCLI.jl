9a0df45b8c33e59b4adbd433cc59627ee2f9f42f
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-02-01 23:33:21 +0100

    add cloning of featured repo with run --featured Refactor

--- src/PlutoCLI.jl
+++ src/PlutoCLI.jl
@@ -1,9 +1,51 @@
-#!<path to your julia executable>
+module PlutoCLI
+
 using Comonicon
 using Pluto
+using LibGit2
+using Pkg
 
-@cast module PlutoCLI
-using Comonicon
+"""
+    run_featured_notebook(port::Int)
+
+Start a new Pluto Notebook in the featured notebook environment.
+"""
+function run_featured_notebook(port::Int)
+    @assert VERSION >= v"1.6" && VERSION < v"1.7" "Featured notebook should be run in Julia 1.6s. Got $VERSION."
+    
+    # Clone the repository into the directory
+    pluto_featured_repo_url = "https://github.com/JuliaPluto/featured"
+    directory_path = joinpath(expanduser("~"), ".julia", "plutocli","envs","featured")
+
+    if !isdir(directory_path)
+        # Clone the repository
+        mkpath(directory_path)
+        LibGit2.clone(pluto_featured_repo_url, directory_path)
+    else
+        repo = LibGit2.GitRepo(directory_path)
+        # Fetch and pull the latest changes
+        LibGit2.fetch(repo)
+    end
+
+
+    cd(joinpath(directory_path,"pluto-deployment-environment"))
+    Pkg.activate(".")
+    Pkg.instantiate()
+
+    # Start the notebook
+    @info "Starting Pluto on port $port..."
+    Pluto.run(;port_hint=port)
+end
+
+"""
+    run_regular_notebook(port::Int)
+
+Start a new regular Pluto Notebook.
+"""
+function run_regular_notebook(port::Int)
+    @info "Starting Pluto on port $port..."
+    Pluto.run(;port_hint=port)
+end
 
 
 """
@@ -14,13 +56,9 @@ Start a new Pluto Notebook.
 @cast function run(; port::Int=1729, featured_port::Int=3458, featured::Bool=false)
 
     if featured
-        @assert VERSION >= v"1.6" && VERSION < v"1.7" "Featured notebook should be run in Julia 1.6. (Update the shebang)"
-
-        # TODO create new project in location with project from /featured
-
-        Pluto.run(;port_hint=featured_port)
+        run_featured_notebook(featured_port)
     else
-        Pluto.run(;port_hint=parse(Int, port))
+        run_regular_notebook(port)
     end
     
 end
@@ -28,7 +66,3 @@ end
 @main
 
 end # PlutoCLI
-
-export PlutoCLI
-
-PlutoCLI.command_main()
